383f5d107e8a83c9296b7c2fbe80cbb9018581cc
Taylor O'Reilly <taylor@taylororeilly.dev>
2026-02-27 18:39:21 -0500

    feat: add victory and defeat jingles
    Add playSequence method for multi-note melodies. Victory plays an
    ascending C-E-G-C arpeggio. Defeat plays a descending G-F-Eb-C
    sawtooth phrase.

--- src/sound.ts
+++ src/sound.ts
@@ -8,20 +8,41 @@ export class SoundEngine {
     return this.ctx;
   }
 
-  private playTone(frequency: number, duration: number, type: OscillatorType = "square") {
+  private playTone(frequency: number, duration: number, type: OscillatorType = "square", volume = 0.1) {
     const ctx = this.getContext();
     const osc = ctx.createOscillator();
     const gain = ctx.createGain();
 
     osc.type = type;
     osc.frequency.value = frequency;
-    gain.gain.setValueAtTime(0.1, ctx.currentTime);
+    gain.gain.setValueAtTime(volume, ctx.currentTime);
     gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + duration);
 
     osc.connect(gain);
     gain.connect(ctx.destination);
     osc.start();
     osc.stop(ctx.currentTime + duration);
+  }
+
+  private playSequence(notes: Array<[number, number]>, type: OscillatorType = "square", volume = 0.1) {
+    const ctx = this.getContext();
+    let time = ctx.currentTime;
+
+    for (const [freq, dur] of notes) {
+      const osc = ctx.createOscillator();
+      const gain = ctx.createGain();
+
+      osc.type = type;
+      osc.frequency.value = freq;
+      gain.gain.setValueAtTime(volume, time);
+      gain.gain.exponentialRampToValueAtTime(0.001, time + dur * 0.9);
+
+      osc.connect(gain);
+      gain.connect(ctx.destination);
+      osc.start(time);
+      osc.stop(time + dur);
+      time += dur;
+    }
   }
 
   paddleHit() {
@@ -34,5 +55,23 @@ export class SoundEngine {
 
   score() {
     this.playTone(220, 0.3, "sawtooth");
+  }
+
+  victory() {
+    this.playSequence([
+      [523, 0.12], // C5
+      [659, 0.12], // E5
+      [784, 0.12], // G5
+      [1047, 0.3], // C6
+    ], "square", 0.08);
+  }
+
+  defeat() {
+    this.playSequence([
+      [392, 0.2],  // G4
+      [349, 0.2],  // F4
+      [311, 0.2],  // Eb4
+      [261, 0.4],  // C4
+    ], "sawtooth", 0.08);
   }
 }
